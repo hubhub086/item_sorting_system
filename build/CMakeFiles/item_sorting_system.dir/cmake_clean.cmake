6da1ad3ce53994270769ec7788f50a3df6152d8b
SongLiang Cao <2023733547@qq.com>
2023-07-12 15:56:18 +0800

    build

---
+++ build/CMakeFiles/item_sorting_system.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/item_sorting_system.dir/main.cpp.o"
+  "CMakeFiles/item_sorting_system.dir/main.cpp.o.d"
+  "CMakeFiles/item_sorting_system.dir/uart.cpp.o"
+  "CMakeFiles/item_sorting_system.dir/uart.cpp.o.d"
+  "item_sorting_system"
+  "item_sorting_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/item_sorting_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/item_sorting_system.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for item_sorting_system.
+# This may be replaced when dependencies are built.
